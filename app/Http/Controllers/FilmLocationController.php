c489e6afcbfdcec800538bbaaa6ddbcd6d33d37f
Wilkins Fernandez <wilkinsfg@gmail.com>
2021-12-05 14:18:53 -0400

    first test for all

--- app/Http/Controllers/FilmLocationController.php
+++ app/Http/Controllers/FilmLocationController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\FilmLocation;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
 
 class FilmLocationController extends Controller
 {
-    //
+    public function index()
+    {
+        $url = 'https://coagisweb.cabq.gov/arcgis/rest/services/public/FilmLocations/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&f=pjson';
+        $client = new Client(); //GuzzleHttp\Client
+        $response = $client->request('GET', $url);
+        return response()->json([
+            'count'    => $response.count(),
+            'productions'      => $response->getBody()
+        ]);
+    }
+    /*public function show(Request $request)
+    {
+        $startDate = Carbon::parse($request->startDate)->toDateTimeString();
+        $endDate = Carbon::parse($request->endDate)->toDateTimeString();
+
+        return FilmLocation::findByDate($startDate, $endDate);
+    }*/
 }
