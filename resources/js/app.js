33abb6e9b662a925b3a97e71f47bf385b29528a1
Wilkins Fernandez <wilkinsfg@gmail.com>
2021-12-05 22:49:13 -0400

    fix models

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,1 @@
 require('./bootstrap');
-require('./moment-with-locales.min');
-console.log(moment().format());
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -15,7 +15,7 @@
                     @foreach($production->sites as $site)
                         <ul>
                             <li>{{ $site->name }}</li>
-                            <li>{{ $site->shoot_date }}</li>
+                            <li>{{ Timezone::convertToLocal(\Carbon\Carbon::createFromTimestampMs($site->shoot_date)) }}</li>
                         </ul>
                     @endforeach
                 @endforeach
