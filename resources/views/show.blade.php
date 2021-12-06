75c65679e75029397d8d81bb154405a1fcfb1d14
Wilkins Fernandez <wilkinsfg@gmail.com>
2021-12-05 21:10:56 -0400

    fix models

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,7 +11,7 @@
             {{$count}}
             <ul>
                 @foreach($productions as $item)
-                    <li>{{ $item['Title'] }}</li>
+                    <li>{{ $item['title'] }}</li>
                 @endforeach
             </ul>
         @endif
